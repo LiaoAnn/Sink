b534d6f739d1665ffe42bd76edca7659be8301fd
LiaoAnn <1050123tom@gmail.com>
2024-11-11 03:08:30 +0000

    chore(build-map): remove Province of China

--- scripts/build-map.js
+++ scripts/build-map.js
@@ -2,4 +2,9 @@ import { writeFileSync } from 'node:fs'
 import { join } from 'node:path'
 import { WorldMapSimplestTopoJSON } from '@unovis/ts/maps.js'
 
+// remove ", Province of China"
+WorldMapSimplestTopoJSON.objects.countries.geometries.forEach((country) => {
+  country.properties.name = country.properties.name.replace(', Province of China', '')
+})
+
 writeFileSync(join(import.meta.dirname, '../public/world.json'), JSON.stringify(WorldMapSimplestTopoJSON), 'utf8')
